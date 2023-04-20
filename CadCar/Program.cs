a7fa58065b649494954e8edf236a97d7dad5474b
Mariana <mariana.daruix@gmail.com>
2023-04-19 22:10:47 -0300

    M02S03EX04

--- CadCar/Program.cs
+++ CadCar/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CadCar;
+
+var proprietario1 = new Proprietario()
+{
+  Nome = "Mariana Gonçalves",
+  Cpf = "565.189.068-80",
+  Telefone = "(11) 98430-7630"
+};
+
+var proprietario2 = new Proprietario()
+{
+  Nome = "Jamil Salim",
+  Cpf = "561.759.458-32",
+  Telefone = "(11) 94109-0149"
+};
+
+var proprietario3 = new Proprietario()
+{
+  Nome = "Davi de Carvalho",
+  Cpf = "126.443.588-68",
+  Telefone = "(11) 97870-9528"
+};
--- CadCar/Proprietario.cs
+++ CadCar/Proprietario.cs
@@ -3,6 +3,9 @@ namespace CadCar
 {
   public class Proprietario
   {
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Telefone { get; set; }
 
   }
 }
